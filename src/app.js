7f93b1090ce4d39ccdc20e318fd519cf94da2a8a
Deep Guchhait <deepguchhait01@gmail.com>
2026-06-23 09:16:56 +0530

    add react in cors

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173/"],
     credentials: true,
   }),
 );
