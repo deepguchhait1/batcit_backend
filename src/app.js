e7de907a369c28ba463196de353b9f1cb279cd2d
Deep Guchhait <deepguchhait01@gmail.com>
2026-06-26 16:46:46 +0530

    fix bugs

--- src/app.js
+++ src/app.js
@@ -18,11 +18,11 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   }),
 );
-
+console.log("view ", process.env.FRONTEND_URL);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -64,7 +64,7 @@ export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      res.status(400).json({ message: "All fileds are required" });
+      return res.status(400).json({ message: "All fileds are required" });
     }
     const user = await User.findOne({ email });
     if (!user) {
